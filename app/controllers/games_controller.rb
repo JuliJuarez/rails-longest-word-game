2880d91174f609bceae5138c34bb120975f5361e
juliana juarez <juulijuarez@gmail.com>
2022-05-19 21:56:02 -0300

    game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    api_url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    user = URI.open(api_url).read
+    contenido = JSON.parse(user)
+    return contenido["found"]
+  end
+end
